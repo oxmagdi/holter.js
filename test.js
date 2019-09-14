89521528da49c569ef612b34f57b69e471ca0cdd
migzz <m.elalfty29@gmail.com>
2019-09-14 21:19:46 +0200

    restructur holerjs & add configs reader service

--- test.js
+++
@@ -1,6 +0,0 @@
-const configReader = require('./src/services/ConfigsReader').configsReader
-
-configReader.getConfigs()
-.then(confs => {
-    console.log(confs)
-}).catch(error => console.error(error))
