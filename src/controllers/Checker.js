36c5472ebe1cca4f74a3041550401778056c4c90
migzzat <migzzat@gmail.com>
2020-03-06 12:47:57 +0200

    DEBUG CHECKER

--- src/controllers/Checker.js
+++ src/controllers/Checker.js
@@ -2,44 +2,44 @@ require('./ConfigsReader')
 
 const http = require('http')
 const NodeModel = require('../models/node')
-const logger = require('../libs/logger/logger') 
+const logger = require('../libs/logger/logger')
 const screech = require('../subscribers/screech')
 const eventEmitter = require('../subscribers/event_emitter')
 
 const INTERVAL_TIME = 2000
-/*******************/
+    /*******************/
 
-function Checker(){}
+function Checker() {}
 
 Checker.prototype.check = async function() {
-    let interval = setInterval( async () => {
+    let interval = setInterval(async() => {
         const nodes = await NodeModel.getAll()
-        if(nodes.length > 0) logger.info()
+        if (nodes.length > 0) logger.info()
         for (const node of nodes) {
             let redisKey = `:${node.cluster}:${node.node}`
             try {
                 await this.seeStatus({
-                  method: 'HEAD', 
-                  host: node.host, 
-                  port: parseInt(node.port), 
-                  path: node.path
+                    method: 'HEAD',
+                    host: node.host,
+                    port: parseInt(node.port),
+                    path: node.path
                 })
 
                 await NodeModel.upStatus(redisKey, 1)
-              //   logger.info(typeof node)
-              //   logger.info(node)
+                    //   logger.info(typeof node)
+                    //   logger.info(node)
                 logger.info(`[${node.cluster}:${node.node}] : ALIVE [${node.domain}:${node.port}]`)
-                
+
             } catch (error) {
                 // this node in critical case
                 const status = await NodeModel.getStatus(redisKey)
-                if(status == -1 || status == 1) {
+                if (status == -1 || status == 1) {
                     await NodeModel.upStatus(redisKey, 0)
                     await screech(node)
                     await eventEmitter.emit('alteration')
                     logger.error(`[${node.cluster}:${node.node}] : DIED [${error.message}]`)
                 } else {
-                   logger.error(`[${node.cluster}:${node.node}] : DIED [${error.message}]`)
+                    logger.error(`[${node.cluster}:${node.node}] : DIED [${error.message}]`)
                 }
             }
         }
@@ -47,12 +47,13 @@ Checker.prototype.check = async function() {
 }
 
 Checker.prototype.seeStatus = function(opt) {
-    return new Promise( (resolve, reject) => {
-        // console.log(opt)
+    return new Promise((resolve, reject) => {
+        console.log("opt : ", opt)
         var options = opt,
-        request = http.request(options, function(req) {
-            resolve()
-        })
+            request = http.request(options, function(req) {
+                console.log("REQ: ", req)
+                resolve()
+            })
 
         request.on('error', function(error) {
             // Handle error
@@ -60,7 +61,7 @@ Checker.prototype.seeStatus = function(opt) {
         })
 
         request.end()
-   })
+    })
 }
 
 module.exports = new Checker()
